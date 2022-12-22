905d3fa0386cb096f5205528bf09247a130313d9
marksmith <mark.smith@hyperoceanic.com>
2022-12-22 09:54:14 +0000

    Add mermaid

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -14,6 +14,11 @@ const config = {
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
 
+  markdown: {
+    mermaid: true,
+  },
+  themes: ['@docusaurus/theme-mermaid'],
+
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'hyperoceanic', // Usually your GitHub org/user name.
